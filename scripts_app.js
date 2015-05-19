8d54200bb8de274c5a25fb649f34443a6dfb4926
rdenubila <rdenubila@gmail.com>
2015-05-19 14:54:36 -0300

    splash

--- scripts_app.js
+++ scripts_app.js
@@ -35,9 +35,9 @@ function checkReady(){
 
 function init(){
 
-	$("#video_intro").html('<video id="video_player" width="100%" height="100%" webkit-playsinline><source src="'+urlVideo+'video.webm" type="video/webm"><source src="'+urlVideo+'video.ogg" type="video/ogg"></video>');
+	$("#video_intro").html('<video id="video_player" width="100%" height="100%" webkit-playsinline><source src="'+urlVideo+'video.mp4" type="video/mp4"><source src="'+urlVideo+'video.webm" type="video/webm"><source src="'+urlVideo+'video.ogg" type="video/ogg"></video>');
 
-	$('body').prepend("<a href='javascript: void(0)' onClick='$(this).fadeOut(); $(\"#video_player\")[0].play();' style='display: block; position: absolute; width: 100%; height: 100%; background: url(images/bgs/"+idioma+"/capa_play.jpg) center center; background-size: cover; z-index: 100;'></a>");
+	$('body').prepend("<a href='javascript: void(0)' id='play_btn' onClick='$(this).fadeOut(); $(\"#video_player\")[0].play();' style='display: block; position: absolute; width: 100%; height: 100%; background: url(images/bgs/"+idioma+"/capa_play.jpg) center center; background-size: cover; z-index: 100;'></a>");
 
 	iniciaVideo();
 
@@ -45,9 +45,11 @@ function init(){
 	//fechaVideo();
 	if(navigator.connection.type!=Connection.WIFI){
 		fechaVideo();
+		$("#play_btn").hide();
 	}
 
 
 }
+
 
 
